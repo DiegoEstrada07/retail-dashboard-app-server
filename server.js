e095539fcf51f0d0a7a569667989199e404a9835
Diego Estrada <diego.estrada1705@gmail.com>
2026-01-13 19:29:15 -0600

    funcional srver coection

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-app.use(cors()); // Middleware para CORS
-app.use(express.json()); // Para parsear JSON
-app.use(express.static('public')); // Si tienes archivos estáticos en 'public'
+app.use(cors()); // Middleware for CORS
+app.use(express.json()); //JSON
+app.use(express.static('public')); // static files in 'public'
 
-// Ruta de ejemplo para API
 app.get('/api/saludo', (req, res) => {
   res.json({ mensaje: '¡Hola desde el backend!' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
+  console.log(`back end server listener http://localhost:${PORT}`);
 });
