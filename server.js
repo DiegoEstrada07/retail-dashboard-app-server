f0aba35e65b55677dae4c7cd0b64ffc4ffb7f345
Diego Estrada <diego.estrada1705@gmail.com>
2026-01-14 04:04:47 -0600

    chore(server.js)sucsessfull conection with the fornt end and creation of the chart:

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { get } = require('node:http');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.use(cors()); // Middleware for CORS
-app.use(express.json()); //JSON
-app.use(express.static('public')); // static files in 'public'
+// Datos de ejemplo que se devolverán en formato JSON
+let data = [
+  { label: 'Apple Watch', value: 10 },
+  { label: 'Samsung Frame', value: 5 },
+  { label: 'Google Nest', value: 100 },
+  { label: 'Mens T-shirt', value: 200 },
+  { label: 'Mens-shorts', value: 200 },
+  { label: 'Womans T-shirt', value: 200 },
+  { label: 'Pillow', value: 20 },
+  { label: 'Towels', value: 20 },
+  { label: 'Microfiber Pillow', value: 20 },
+  { label: 'Soap', value: 50 },
+  { label: 'Irish Spring', value: 50 },
+  { label: 'NIVEA', value: 50 },
+  { label: 'LEGO', value: 30 },
+  { label: 'Silverstone', value: 30 },
+  { label: 'Chinise Horse', value: 30 }
+];
 
-app.get('/api/saludo', (req, res) => {
-  res.json({ mensaje: '¡Hola desde el backend!' });
+app.use(cors()); // Esto habilita CORS para todas las rutas
+// Ruta para obtener los datos en formato JSON
+app.get('/api/data', (req, res) => {
+  res.json(data);
 });
 
-app.listen(PORT, () => {
-  console.log(`back end server listener http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
